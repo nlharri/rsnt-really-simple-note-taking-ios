f96ef5e68c30441874eed3232187fbb19e491253
László Harri Németh <nlharri@gmail.com>
2019-01-19 08:25:31 +0100

    initial commit

---
+++ rsnt-really-simple-note-taking-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rsnt-really-simple-note-taking-ios
+//
+//  Created by Németh László Harri on 2019. 01. 19..
+//  Copyright © 2019. Németh László Harri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
