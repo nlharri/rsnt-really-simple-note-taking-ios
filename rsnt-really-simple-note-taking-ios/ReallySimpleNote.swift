1476137fd90937ef94eb04072a7b163bfc07e200
László Harri Németh <nlharri@gmail.com>
2019-01-27 07:59:18 +0100

    enhance data model with UUID

--- rsnt-really-simple-note-taking-ios/ReallySimpleNote.swift
+++ rsnt-really-simple-note-taking-ios/ReallySimpleNote.swift
@@ -6,12 +6,23 @@
 //  Copyright © 2019. Németh László Harri. All rights reserved.
 //
 
+import Foundation
+
 class ReallySimpleNote {
+    private(set) var noteId    : UUID
     private(set) var noteTopic : String
     private(set) var noteText  : String
     private(set) var noteDate  : String
 
     init(noteTopic:String, noteText:String, noteDate:String) {
+        self.noteId    = UUID()
+        self.noteTopic = noteTopic
+        self.noteText  = noteText
+        self.noteDate  = noteDate
+    }
+
+    init(noteId: UUID, noteTopic:String, noteText:String, noteDate:String) {
+        self.noteId    = noteId
         self.noteTopic = noteTopic
         self.noteText  = noteText
         self.noteDate  = noteDate
